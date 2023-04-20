5cd4f231a3bb5fb70ccab265d83ffa928402750f
Ryan Lanziloti De Faria Teixeira <pitagoraslanz@gmail.com>
2023-04-20 17:20:46 -0300

    Revert "Tela Login"
    This reverts commit c05a7ce168f6131591060e413b4e9fd4bf88929f.

--- src/main/java/com/fullmadagilists/api2semestre/API2Semestre.java
+++ src/main/java/com/fullmadagilists/api2semestre/API2Semestre.java
@@ -1,13 +1,13 @@
 package com.fullmadagilists.api2semestre;
 
-import com.fullmadagilists.api2semestre.telas.Login;
+import com.fullmadagilists.api2semestre.telas.TelaLogin;
 
 public class API2Semestre {
 
     public static void main(String[] args) {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new TelaLogin().setVisible(true);
             }
         });
     }
