97c8bf431fecab3aef532c7bdf8aed4752635171
Cauana Dias <cauana.dias@hotmail.com>
2023-04-21 01:04:45 -0300

    login

--- src/main/java/com/fullmadagilists/api2semestre/API2Semestre.java
+++ src/main/java/com/fullmadagilists/api2semestre/API2Semestre.java
@@ -1,10 +1,13 @@
 package com.fullmadagilists.api2semestre;
 
+import com.fullmadagilists.api2semestre.comum.ConexaoBancoDeDados;
 import com.fullmadagilists.api2semestre.telas.Login;
 
 public class API2Semestre {
 
     public static void main(String[] args) {
+        
+        ConexaoBancoDeDados.usuarios();
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Login().setVisible(true);
